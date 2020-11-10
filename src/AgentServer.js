2c0584946759aa1f6eca931864f6e15f59afe416
Arthur <arturs.grustins@gmail.com>
2020-11-10 10:38:46 +0000

    Cleanup

--- src/AgentServer.js
+++ src/AgentServer.js
@@ -1,7 +1,5 @@
 import Proxy from "@core/Proxy";
 import { AgentServer, Database } from "@config/index";
-import * as ctrl from "@control/index";
-import * as svc from "@service/index";
 
 const module = new Proxy({
   ...AgentServer,
