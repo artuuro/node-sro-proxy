c7e6b87838fd213b7067c6b866d75454e43d4217
Arthur G <arturs.grustins@gmail.com>
2021-01-20 16:32:36 +0000

    Added example for redirects

--- src/config/Redirects.js
+++ src/config/Redirects.js
@@ -4,6 +4,10 @@ export default {
         host: process.env.REDIRECT_AGENT_IP || '127.0.0.1', // <-- IP TO
         port: process.env.REDIRECT_AGENT_PORT || 8002, // <-- PORT TO
     },
+    // ':14': { // <-- This would be another agent or download.. depends what you need.
+    //     host: process.env.REDIRECT_AGENT_IP || '127.0.0.2', // <-- IP TO
+    //     port: process.env.REDIRECT_AGENT_PORT || 8002, // <-- PORT TO
+    // },
     // DownloadServer
     '138.201.58.79:16002': { // <-- IP FROM
         host: process.env.REDIRECT_DOWNLOAD_IP || '127.0.0.1', // <-- IP TO
